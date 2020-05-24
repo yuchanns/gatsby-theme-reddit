053707ff3678b1edadd24d3960d9927efd4e439a
yuchanns <airamusume@gmail.com>
2020-05-24 16:41:18 +0800

    improve page not-found

--- reddit/layouts/not-found.js
+++ reddit/layouts/not-found.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import Header from '../components/header'
+import styles from '../styles/layout.module.scss'
+import { Link } from 'gatsby'
+import SEO from '../components/seo'
 
-const NotFound = () => {
+const NotFound = ({location}) => {
   return (
-    <div>404</div>
+    <div>
+      <Header location={location}></Header>
+      <div className={styles.container}>
+        <SEO title="404 not found" />
+        <div className={styles.containerOuterNotfound}>
+          <h3 className={styles.containerNotfound}>Sorry, there doesn't seem to be anything here.</h3>
+          <Link className={styles.containerNotfoundBtn} to="/">Go Home</Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
