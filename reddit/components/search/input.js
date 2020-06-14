10b0947ca6032ffd176e567b507aab1aafc224cb
yuchanns <airamusume@gmail.com>
2020-06-14 19:41:19 +0800

    improve search box to add algolia icon

--- reddit/components/search/input.js
+++ reddit/components/search/input.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import { connectSearchBox } from 'react-instantsearch-dom'
+import { connectSearchBox, PoweredBy } from 'react-instantsearch-dom'
 import styles from '../../styles/header.module.scss'
 
 export default connectSearchBox(({ refine, setShow }) => (
-  <input
+  <>
+    <input
     className={styles.headerSearchInput}
     type="search"
     id="reddit-header-search"
     onChange={e => refine(e.target.value)}
     onFocus={() => setShow(true)}
-    autocomplete="off"
-    placeholder="Search Powered By Algolia"/>
+    autoComplete="off"
+    placeholder="Search" />
+    <PoweredBy />
+  </>
 ))
